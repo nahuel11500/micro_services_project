36db04696ea7bfdf9b7060974b7657e862a2bb94
alphakuka <projetrobot11@gmail.com>
2023-09-20 11:23:58 +0200

    Added the necessary modification to the showtime.py.

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,19 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods = ['GET'])
+def get_showtime():
+   res = make_response(jsonify(schedule), 200)
+   return res
+
+@app.route("/showmovies/<date>", methods = ['GET'])
+def get_movie_date(date):
+   for dates in schedule:
+      if str(dates['date']) == str(date):
+         res = make_response(jsonify(dates), 200)
+         return res
+   return make_response(jsonify({"error": "Date not found"}), 400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
