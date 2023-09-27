afee86439d46ad7b143c4990fce9420b16f57f8f
alphakuka <projetrobot11@gmail.com>
2023-09-27 11:39:47 +0200

    Updated a bit the yaml for the user spefication and added working code in user.py

--- user/user.py
+++ user/user.py
@@ -15,7 +15,38 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/showusers", methods=['GET'])
+def get_users():
+   return make_response(jsonify(users),200)
+
+@app.route('/show/<user_name>', methods=["GET"])
+def get_user(user_name):
+   for _user in users:
+      if str(_user['name']) == str(user_name):
+         res = make_response(jsonify(_user), 200)
+         return res
+   return make_response(jsonify({"error": "User not found"}), 400)
+
+@app.route('/show/reservation/user/<user_name>', methods=['GET'])
+def get_user_reservation_by_name(user_name):
+    ## Get the id with the name
+    for _user in users:
+        if str(_user['name']) == str(user_name):
+            user_id = _user["id"]
+            # Make a request to see if the date is available for the movie
+            response = requests.get(f'http://172.16.134.102:3201/bookings/{user_id}')
+            if response.status_code == 200:
+                merged_dict = {**_user, **response.json()}
+                return make_response(jsonify(merged_dict), 200)
+            else:
+                return make_response(jsonify({"error": "Booking information not found"}), 400)
+
+    return make_response(jsonify({"error": "User not found"}), 400)
+
+
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
+
